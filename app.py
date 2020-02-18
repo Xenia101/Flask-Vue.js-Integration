43b06b2fcdef3d683d9d45a10d1a2dce590629e8
Xenia <lommy101@gmail.com>
2020-02-18 18:46:02 +0900

    add Vue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-class CustomFlask(flask):
+class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
     jinja_options.update(dict(
         block_start_string='(%',
@@ -12,3 +12,12 @@ class CustomFlask(flask):
     ))
 
 app = CustomFlask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(host='localhost', port=5000)
+    
